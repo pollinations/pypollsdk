a3d71d33b6fce473d5ac7a7119b49bd36b836457
Niels Warncke <niels.warncke@gmail.com>
2022-09-21 20:13:11 +0200

    fix error

--- pypollsdk/constants.py
+++ pypollsdk/constants.py
@@ -10,9 +10,10 @@ load_dotenv()
 try:
     url: str = os.environ.get("SUPABASE_URL")
     supabase_api_key: str = os.environ.get("SUPABASE_API_KEY")
-    supabase_id: str = os.get("SUPABASE_ID")
+    supabase_id: str = os.environ.get("SUPABASE_ID")
     supabase: Client = create_client(url, supabase_api_key)
 except:  # noqa
+    breakpoint()
     logging.error(
         "Could not connect to Supabase. This is required for the SDK to work."
     )
