ffa0d8556e6838d9044b8a0023b1d93f5340623a
Niels Warncke <niels.warncke@gmail.com>
2022-09-25 14:32:41 +0200

    add timeoutdecorator dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ setup(
     packages=find_packages(),
     package_data={},
     scripts=[],
-    install_requires=["supabase", "python-dotenv", "pytest", "wget"],
+    install_requires=[
+        "supabase",
+        "python-dotenv",
+        "pytest",
+        "wget",
+        "timeout-decorator",
+    ],
     extras_require={
         "test": ["pytest", "pylint!=2.5.0", "black", "mypy", "flake8", "pytest-cov"],
     },
