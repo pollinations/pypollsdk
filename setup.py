48dda73a1f22c1387a3bc08a967e01d75b855bcd
Niels Warncke <niels.warncke@gmail.com>
2022-09-26 12:05:58 +0200

    set all versions of dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ setup(
     packages=find_packages(),
     package_data={},
     scripts=[],
-    install_requires=["supabase", "python-dotenv", "pytest", "wget", "httpx>=0.21.3"],
+    install_requires=[
+        "supabase==0.5.8",
+        "python-dotenv==0.20.0",
+        "pytest==7.1.2",
+        "wget==3.2",
+        "httpx==0.21.3",
+    ],
     extras_require={
         "test": ["pytest", "pylint!=2.5.0", "black", "mypy", "flake8", "pytest-cov"],
     },
